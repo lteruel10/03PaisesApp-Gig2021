42e0baa704bba609b4f29d90bf72b4eea04acfbe
lteruel10 <lteruel10@gmail.com>
2021-10-07 21:41:37 -0600

    103. Componente para buscar por país

--- src/app/app-routing.modulte.ts
+++ src/app/app-routing.modulte.ts
@@ -7,6 +7,7 @@ import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.compon
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
+import { SharedModule } from './shared/shared.module';
 
 export const routes: Routes = [
     {
@@ -35,7 +36,6 @@ export const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(routes)
-
     ],
     exports: [
         RouterModule
